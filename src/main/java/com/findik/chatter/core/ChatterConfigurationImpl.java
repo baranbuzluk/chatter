ca6b6ffa044c9a82b6ef5ad33db90f88a88d71c0
Baran Buzluk <baranbuzluk@gmail.com>
2022-05-28 14:43:32 +0300

    Properties.dosyası kaldırıldı.

--- src/main/java/com/findik/chatter/core/ChatterConfigurationImpl.java
+++ src/main/java/com/findik/chatter/core/ChatterConfigurationImpl.java
@@ -1,50 +1,29 @@
 package com.findik.chatter.core;
 
-import java.io.FileInputStream;
 import java.nio.file.Path;
-import java.util.Properties;
 
 import org.springframework.stereotype.Component;
-
-import com.findik.chatter.enums.ChatterConfigurationKey;
 
 @Component
 public class ChatterConfigurationImpl implements ChatterConfiguration {
 
-	private static final String RESOURCES_CONFIG_PROPERTIES = "./resources/config.properties";
-
-	private final Properties properties;
-
-	public ChatterConfigurationImpl() {
-		properties = new Properties();
-		try (FileInputStream fis = new FileInputStream(RESOURCES_CONFIG_PROPERTIES)) {
-			properties.load(fis);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
 	@Override
 	public Path getMessageOutputDirectory() {
-		String messageOutputPath = properties.getProperty(ChatterConfigurationKey.MESSAGE_OUTPUT_PATH.getKey());
-		return Path.of(messageOutputPath);
+		return Path.of("C:/CHATTER/");
 	}
 
 	@Override
 	public String getClientIp() {
-		return properties.getProperty(ChatterConfigurationKey.CLIENT_IP.getKey());
+		return "127.0.0.1";
 	}
 
 	@Override
 	public String getClientPort() {
-		return properties.getProperty(ChatterConfigurationKey.CLIENT_PORT.getKey());
+		return "9999";
 	}
 
 	@Override
 	public Integer getConnectTimeout() {
-		String value = properties.getProperty(ChatterConfigurationKey.CONNECT_TIMEOUT.getKey());
-		if (value == null)
-			throw new PropertiesKeyNotFoundException(ChatterConfigurationKey.CONNECT_TIMEOUT.getKey());
-		return Integer.parseInt(value);
+		return 1000;
 	}
 }

6a6cf48e2b5b57bbdfe07c98b4c060b77d946eb9
Baran Buzluk <baranbuzluk@gmail.com>
2022-06-18 20:11:04 +0300

    MainViewServiceImpl refactor edildi.

--- client/src/main/java/com/chatter/client/main/MainViewServiceImpl.java
+++ client/src/main/java/com/chatter/client/main/MainViewServiceImpl.java
@@ -1,7 +1,6 @@
 package com.chatter.client.main;
 
 import java.util.Objects;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -18,34 +17,38 @@ import javafx.stage.Stage;
 @Component
 public class MainViewServiceImpl implements MainViewService {
 
-	@Autowired
 	private EventManager eventManager;
 
-	private Optional<Stage> mainStageOptional = Optional.empty();
+	private Stage mainStage;
 
 	private Scene mainScene;
 
+	@Autowired
+	public MainViewServiceImpl(EventManager eventManager) {
+		this.eventManager = eventManager;
+	}
+
 	@Override
 	public void show(Pane pane) {
-		Objects.requireNonNull(pane);
-		mainStageOptional.ifPresent(mainStage -> {
+		Objects.requireNonNull(pane, "pane can not be null!");
+		if (mainStage != null) {
 			mainScene.setRoot(pane);
 			mainStage.sizeToScene();
 			mainStage.show();
 			mainStage.centerOnScreen();
 			mainStage.toFront();
-		});
+		}
 	}
 
 	@Override
 	public void setMainStage(Stage stage) {
-		mainStageOptional = Optional.ofNullable(stage);
-		mainStageOptional.ifPresent(mainStage -> {
-			StackPane dummy = new StackPane();
-			this.mainScene = new Scene(dummy);
-			mainStage.setScene(mainScene);
-			eventManager.sendEvent(new EventInfo(ClientEvent.STARTED_APPLICATION));
-		});
+		mainStage = Objects.requireNonNull(stage, "stage can not be null!");
+		StackPane dummy = new StackPane();
+		this.mainScene = new Scene(dummy);
+		mainStage.setScene(mainScene);
+		EventInfo startedApplicationEvent = new EventInfo(ClientEvent.STARTED_APPLICATION);
+		eventManager.sendEvent(startedApplicationEvent);
+
 	}
 
 }

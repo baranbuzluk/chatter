7e47529aa4d4e1e69fa5149c81f141fdece5cf63
Baran Buzluk <baranbuzluk@gmail.com>
2022-06-25 14:39:53 +0300

    IO işlemleri için abstract bir sınıfı oluşturuldu.

--- core/src/main/java/com/chatter/core/socket/ChatterSocket.java
+++ core/src/main/java/com/chatter/core/socket/ChatterSocket.java
@@ -1,21 +1,12 @@
 package com.chatter.core.socket;
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.net.Socket;
-import java.nio.charset.StandardCharsets;
 
-public class ChatterSocket {
+public class ChatterSocket extends IOHandler {
 
 	private Socket socket;
 
-	private PrintWriter printWriter;
-
 	private final MessageListener messageListener;
-
-	private volatile boolean runLoop;
 
 	public ChatterSocket(MessageListener messageListener) {
 		this.messageListener = messageListener;
@@ -28,11 +19,8 @@ public class ChatterSocket {
 
 		try {
 			socket = new Socket(ip, port);
-			printWriter = new PrintWriter(socket.getOutputStream(), true, StandardCharsets.UTF_8);
-			runLoop = true;
-			Thread thread = new Thread(new RunLoop(), "Message Listener-Thread");
-			thread.setDaemon(true);
-			thread.start();
+			setOutputStream(socket.getOutputStream());
+			setInputStream(socket.getInputStream());
 			return true;
 		} catch (Exception e) {
 			System.err.println(e.getLocalizedMessage());
@@ -47,13 +35,10 @@ public class ChatterSocket {
 		}
 
 		try {
-			runLoop = false;
 			socket.shutdownInput();
 			socket.shutdownOutput();
 			socket.close();
-
 			socket = null;
-			printWriter = null;
 			return true;
 		} catch (Exception e) {
 			System.err.println(e.getLocalizedMessage());
@@ -66,35 +51,9 @@ public class ChatterSocket {
 		return socket != null && socket.isConnected() && !socket.isClosed();
 	}
 
-	public boolean sendMessage(String message) {
-		if (printWriter != null) {
-			printWriter.println(message);
-			return true;
-		}
-		return false;
-	}
-
-	class RunLoop implements Runnable {
-		@Override
-		public void run() {
-			if (socket == null) {
-				return;
-			}
-			try {
-				InputStream inputStream = socket.getInputStream();
-				BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
-				while (runLoop) {
-					String readLine;
-					if ((readLine = reader.readLine()) != null) {
-						messageListener.receivedMessage(readLine);
-					}
-				}
-				reader.close();
-				inputStream.close();
-			} catch (Exception e) {
-				System.err.println(e.getLocalizedMessage());
-			}
-		}
+	@Override
+	protected void receivedMessage(String message) {
+		messageListener.receivedMessage(message);
 	}
 
 }

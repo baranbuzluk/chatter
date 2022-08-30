6fc812da96197f1a2d1fb395c15dfb283ecffd90
burakcantemur <burakcantemur@outlook.com>
2022-08-30 13:05:09 +0300

    Login ekranına Remember me özelliği eklendi

--- client/src/main/java/com/chatter/client/controller/login/LoginController.java
+++ client/src/main/java/com/chatter/client/controller/login/LoginController.java
@@ -16,6 +16,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
@@ -34,6 +35,8 @@ public class LoginController extends AbstractController<LoginService> implements
 
 	@FXML
 	private TextField usernameTextField;
+	@FXML
+	private CheckBox rememberMeBox;
 
 	public LoginController(LoginService service) {
 		super("Login.fxml", service);

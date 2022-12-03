469287b3c568999ad1264027c5aba8eab5267678
Baran Buzluk <baranbuzluk@gmail.com>
2022-12-03 16:21:39 +0300

    Programın birden fazla kez açılmaması sağlandı.

--- src/main/java/com/chatter/MainClass.java
+++ src/main/java/com/chatter/MainClass.java
@@ -1,8 +1,47 @@
 package com.chatter;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.channels.FileChannel;
+import java.nio.channels.FileLock;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
 public class MainClass {
 
+	private static FileLock tryLock;
+
+	private static RandomAccessFile randomAccessFile;
+
 	public static void main(String[] args) {
+		checkIfProgramIsAlreadyRunning();
 		MainFX.main(args);
+	}
+
+	private static void checkIfProgramIsAlreadyRunning() {
+		File tempFile = new File(System.getProperty("java.io.tmpdir"), "chatter.tmp");
+		try {
+			tempFile.createNewFile();
+		} catch (IOException e1) {
+			JOptionPane.showMessageDialog(new JFrame(), "Failed to create temporary file", "Error",
+					JOptionPane.ERROR_MESSAGE);
+			System.exit(0);
+		}
+
+		try {
+			randomAccessFile = new RandomAccessFile(tempFile, "rw");
+			FileChannel fileChannel = randomAccessFile.getChannel();
+			tryLock = fileChannel.tryLock();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		if (tryLock == null) {
+			JOptionPane.showMessageDialog(new JFrame(), "The program is already running", "Error",
+					JOptionPane.ERROR_MESSAGE);
+			System.exit(0);
+		}
 	}
 }
